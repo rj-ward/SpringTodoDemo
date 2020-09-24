6863a83df5d581dd48a8cb6c6b041af88504ac74
aj-ward <59932988+rj-ward@users.noreply.github.com>
2020-09-24 16:55:58 -0500

    Initial commit

---
+++ src/main/java/com/example/SpringTodoDemo/SpringTodoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringTodoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringTodoDemo/SpringTodoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTodoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
